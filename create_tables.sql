a699be3528fbb11c6f617d457f9a2061088e8350
u1234x1234 <u1234x1234@gmail.com>
2016-05-09 23:44:38 +0500

    init

---
+++ create_tables.sql
@@ -0,0 +1,14 @@
+create table ItemInfo_train
+(
+itemID int, 
+categoryID int,
+title varchar,
+description varchar,
+images_array varchar,
+attrsJSON varchar,
+price double precision, 
+locationID int,
+metroID double precision,
+lat double precision,
+lon double precision
+); 
